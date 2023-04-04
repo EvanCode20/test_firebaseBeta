b564c7b46db831aaa7f2db7ad3c28e9c35155c6d
EvanCode20 <evan.teague11@gmail.com>
2023-04-04 06:58:05 +0000

    initial commit

---
+++ app/src/main/java/com/test/test_firebase/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.test.test_firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtUsername = findViewById<EditText>(R.id.etUsername)
+        val pwPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        val inputUsername = txtUsername.text.toString()
+        val inputPassword = pwPassword.text.toString()
+
+        auth = Firebase.auth
+
+        btnLogin.setOnClickListener {
+            auth.createUserWithEmailAndPassword(inputUsername, inputPassword)
+                .addOnCompleteListener(this)
+                { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+                    }
+                }
+                .addOnFailureListener(this)
+                {
+                    exception ->
+                    Toast.makeText(this,exception.message, Toast.LENGTH_LONG).show()
+                }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
